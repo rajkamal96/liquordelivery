77e6d8267b2cb3a10e2bf3f64ea33feee7ad8fca
Raj <coolrajkamal22@gmail.com>
2020-06-23 14:00:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
-import Product from './components/Product';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
